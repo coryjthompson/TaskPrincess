b918b6bd2cd09c6e08ba2832da159c3c608c9ded
Cory Thompson <cory@coryjthompson.com>
2020-03-21 14:42:32 +1100

    Add binary operators

--- tests/TaskPrincess.FilterDsl.UnitTests/FilterLexerTests.cs
+++ tests/TaskPrincess.FilterDsl.UnitTests/FilterLexerTests.cs
@@ -8,8 +8,8 @@ namespace TaskPrincess.FilterDslTest
 {
     public class FilterLexerTests
     {
-        private readonly MockItem _adventureProject = new MockItem() { Uuid = Guid.Parse("7688bae9-4e72-4630-a553-a3b0d9ffb3bb"), Id=5, Project = "adventure" };
-        private readonly MockItem _castleProject = new MockItem() { Uuid = Guid.Parse("e7be5c17-4f4e-45f4-af4b-bd87a9ad9d87"), Id=10, Project = "castle" };
+        private readonly MockItem _adventureProject = new MockItem() { Uuid = Guid.Parse("7688bae9-4e72-4630-a553-a3b0d9ffb3bb"), Id = 5, Project = "adventure" };
+        private readonly MockItem _castleProject = new MockItem() { Uuid = Guid.Parse("e7be5c17-4f4e-45f4-af4b-bd87a9ad9d87"), Id = 10, Project = "castle" };
 
         [Fact]
         public void TestQuery_EqualsOnQuotedString()
@@ -83,6 +83,50 @@ namespace TaskPrincess.FilterDslTest
             Assert.False(query.Invoke(_castleProject));
         }
 
+        [Fact]
+        public void TestQuery_BinaryAndOperator()
+        {
+            var query = BuildQuery("project:castle AND id:5");
+            Assert.False(query.Invoke(_adventureProject));
+            Assert.False(query.Invoke(_castleProject));
+
+            var query2 = BuildQuery("project:castle and id:10");
+            Assert.False(query2.Invoke(_adventureProject));
+            Assert.True(query2.Invoke(_castleProject));
+        }
+
+        [Fact]
+        public void TestQuery_BinaryImpliedAndOperator()
+        {
+            var query = BuildQuery("project:castle id:10");
+            Assert.False(query.Invoke(_adventureProject));
+            Assert.True(query.Invoke(_castleProject));
+        }
+
+        [Fact]
+        public void TestQuery_BinaryOrOperator()
+        {
+            var query = BuildQuery("project:castle OR id:5");
+            Assert.True(query.Invoke(_adventureProject));
+            Assert.True(query.Invoke(_castleProject));
+
+            var query2 = BuildQuery("project:nothing or id:10");
+            Assert.False(query2.Invoke(_adventureProject));
+            Assert.True(query2.Invoke(_castleProject));
+        }
+
+        [Fact]
+        public void TestQuery_BinaryXorOperator()
+        {
+            var query = BuildQuery("project:castle XOR id:5");
+            Assert.True(query.Invoke(_adventureProject));
+            Assert.True(query.Invoke(_castleProject));
+
+            var query2 = BuildQuery("project:castle xor id:10");
+            Assert.False(query2.Invoke(_adventureProject));
+            Assert.False(query2.Invoke(_castleProject));
+        }
+
         private Func<MockItem, bool> BuildQuery(string text)
         {
             var inputStream = new AntlrInputStream(text);
@@ -101,6 +145,6 @@ namespace TaskPrincess.FilterDslTest
     {
         public string Project { get; set; }
         public Guid Uuid { get; set; }
-        public int Id {get; set;}
+        public int Id { get; set; }
     }
 }
